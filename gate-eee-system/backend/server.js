7c3f3c37e99e1640837ee949a508c6761a692a5f
manojvel210406 <itzmemanojvelmurugan@gmail.com>
2026-05-05 17:24:48 +0530

    Update server.js

--- gate-eee-system/backend/server.js
+++ gate-eee-system/backend/server.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 // ─── Database Connection ──────────────────────────────────────────────────
-import mongoose from "mongoose";
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
